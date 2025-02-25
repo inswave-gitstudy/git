0c43b637e4c42a7396c22333150a99847c5b9ba4
dorazi0423 <dorazi0423@gmail.com>
2025-02-25 20:04:17 +0900

    second gdi commit

--- src/week2/Test_gdi.java
+++ src/week2/Test_gdi.java
@@ -1,8 +1,8 @@
 package week2;
 
-public class Test {
+public class Test_gdi {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Test 파일입니다");
     }
 }
 
